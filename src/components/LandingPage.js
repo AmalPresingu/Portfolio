836e9caf0ef8a29b3a194d0df5b7ea5674f9a94e
AmalPresingu <amalpresingu10@gmail.com>
2024-01-06 23:19:53 -0600

    working version (replace slide fade with chakraUI)

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,20 +1,34 @@
 import './LandingStyles.css'
 import '../App.css'
 import TypingReveal from './TypingReveal';
-import Fade from './Fade'
+import { useEffect, useState } from 'react';
+
 
 const LandingPage = () => {
+    const [showDiv, setShowDiv] = useState(false);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setShowDiv(true);
+        }, 3850);
+        return () => clearTimeout(timer);
+    }, [])
     return (
-        <div className='landing'>
+        <>
             <h1 className="greeting pixel">
                 <TypingReveal text="> Hello world, my name is" speed={100}/>
             </h1>
-            <Fade delay={3500}>
+            <div className="landing">
+                {showDiv && (
+                <>
                 <h1 className="name helvetica">Amal Presingu.</h1>
                 <h2 className="role helvetica">Software Engineer <span className='role-desc'>specializing in Web Development.</span></h2>
-            </Fade>
-        </div>
-    )
-}
+                </>
+            )}
+            </div>
+        </>
+      );
+    }
 
 export default LandingPage;
+  
