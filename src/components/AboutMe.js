6ec8258b9ba63af24500474916d4a3e85f9ebbfe
AmalPresingu <amalpresingu10@gmail.com>
2024-01-12 22:39:26 -0600

    Completed AboutMe
    - make resume button and greeting responsive
    - fade in aboutme
    - add projects and contact
    - fix navbar on scroll

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -9,7 +9,23 @@ const AboutMe = () => {
         <div className='about-container'>
             <h1 className='label inter'>About Me</h1>
             <div className='p-container'>
-                <p className='sora'>I’m a software engineer specializing in web development. My coding journey began in middle school when I began playing Minecraft, my first PC game. I was fascinated with the CLI and how it interacted with the game. </p>
+                <p className='sora'>
+                My coding journey began in middle school when I began playing Minecraft, my first PC game. 
+                I was fascinated with the CLI and how it interacted with the game. I didn't know it at the time,
+                but it was my gateway into the world of programming.<br/><br/>
+                In December of 2022, I graduated with a B.S. in Computer Science.
+                After graduating a semester early, I took a gap year to seek out a path that aligned with
+                my interests and goals. <br/><br/>
+                As I continue to learn and develop my skills, I'm looking for opportunities preferrably in web development. 
+                Here are some of the technologies I've been working with: <br/><br/></p>
+                <ul className='tech-list pixel'>
+                    <li>JavaScript</li>
+                    <li>React</li>
+                    <li>HTML5</li>
+                    <li>CSS</li>
+                    <li>Unity</li>
+                    <li>C#</li>
+                </ul>
             </div>
         </div>
     );
